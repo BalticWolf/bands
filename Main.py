3dc874fe46e635d14eece58200452279ca550429
Timothée <timothee.aupetit@gmail.com>
2017-10-29 14:55:59 +0100

    commented Main methods

--- Main.py
+++ Main.py
@@ -14,13 +14,15 @@ def main():
     # make_url_file('./data/rym_extract')
     # with open('./data/urls', 'r') as f:
     #     f.readline()
-    # neo = NeoDb("bolt://127.0.0.1:7687", "neo4j", "bands")
-    # session = neo.get_session()
+    neo = NeoDb("bolt://127.0.0.1:7687", "neo4j", "bands")
+    session = neo.get_session()
     # for band in Band.get_all(session):
     #     print(band["name"])
 
     band = get_info_from_url('./data/pages/Black Sabbath - Sonemic _ Rate Your Music music database.html')
     print(band)
+
+    print(Band.insert(session, band))
 
 
 def make_url_file(path):
@@ -67,6 +69,10 @@ def make_url(band_name):
 
 
 def get_info_from_url(link):
+    """
+    :param link: file or link to handle
+    :return: {} representing a band
+    """
     page = PyQuery(filename=link)
 
     # extract band name
@@ -82,7 +88,7 @@ def get_info_from_url(link):
     data1 = [item.text for item in info('div.artist_page_info_data')]
     data2 = [item.text() for item in info('div.artist_page_info_data').items('p')]
 
-    # gather information: replace trailing None values of elts1 by elts2
+    # merge information: replace trailing None values of data1 by data2
     items = data1[:-len(data2)] + data2
 
     # create key-value pairs
@@ -94,12 +100,17 @@ def get_info_from_url(link):
         if key in band:
             band[key] = band[key].strip(' ').strip(',')
 
+    # format data
     band['Members'] = to_dict_list(band['Members'])
 
     return band
 
 
 def to_dict_list(raw_members):
+    """
+    :param raw_members: string containing all band members and their features
+    :return: [{}] representing all members and their features
+    """
     keys = ['Name', 'Instruments', 'Periods']
 
     # get a list of band members, ignoring the last parenthesis of raw_members
@@ -123,32 +134,41 @@ def to_dict_list(raw_members):
             if i == 2:
                 member[i] = transform_periods(member[i])
 
-
     # transform members as list of dict
     members = [dict(zip(keys, member)) for member in members]
 
-    print(members)
     return members
 
 
 def transform_periods(raw_periods):
+    """
+    :param raw_periods: string representing all the activity periods of a band member
+    :return: [{'Start': ... , 'End': ... }, ... ] representing all the activity periods
+    """
     periods = raw_periods
     for i in range(len(periods)):
         periods[i] = transform_period(periods[i])
 
-    print(periods)
     return periods
 
 
 def transform_period(raw_period):
+    """
+    :param raw_period: string representing a period of activity of a band member
+    :return: {'Start': ... , 'End': ... } or {'Start': ... } representing the activity period
+    """
     limits = raw_period.split('-')
 
+    # No end date: the member started and stopped activity the same year
     if len(limits) == 1:
         period = {'Start': int(limits[0]), 'End': int(limits[0])}
 
-    if len(limits) == 2:
+    # End date section specified
+    elif len(limits) == 2:
         year = limits[1]
+
         if year not in ['present', 'pres', 'pres.', 'current']:
+            # the period of activity is over
             year = int(year)
 
             # force year to be written on 4 digits
@@ -160,7 +180,11 @@ def transform_period(raw_period):
             period = {'Start': int(limits[0]), 'End': year}
 
         else:
+            # the member is still currently active
             period = {'Start': int(limits[0])}
+
+    else:
+        period = {}
 
     return period
 
