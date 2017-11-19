8d893c7e15c50dd2c5578b5518ffe65649755afb
Timothée <timothee.aupetit@gmail.com>
2017-11-19 22:47:04 +0100

    moved code from Main to MakeUrl script

--- Main.py
+++ Main.py
@@ -6,7 +6,6 @@ import os
 import re
 from datetime import date
 
-import pandas as pd
 from pyquery import PyQuery
 
 from DbConnection import NeoDb
@@ -37,49 +36,6 @@ def main():
                 print(band)
                 band.insert(session)
     session.close()
-
-
-def make_url_file(path):
-    rym = pd.read_csv(path, encoding='utf-8')
-    rym.columns = rym.columns.map(lambda x: x.strip(' '))
-    rym = rym.iloc[:, 1:3]
-    rym['First Name'] = rym['First Name'].apply(str)
-    rym = rym.drop_duplicates()
-
-    artists = rym.apply(lambda row: row[0] + ' ' + row[1], axis=1)
-    artists = artists.map(lambda x: x.replace('nan ', ''))
-
-    urls = artists.map(lambda x: make_url(x))
-    urls.to_csv('./data/urls', header=False, index=False)
-
-
-def make_url(band_name):
-    base_url = "https://rateyourmusic.com/artist/"
-
-    name = band_name\
-        .replace(' O)))', '-o') \
-        .replace(' ', '_') \
-        .replace('/', '_') \
-        .replace(',', '_') \
-        .replace('&amp;', 'and') \
-        .lower()\
-        .replace("'", "")\
-        .replace('à', 'a')\
-        .replace('â', 'a')\
-        .replace('ä', 'a')\
-        .replace('è', 'e')\
-        .replace('é', 'e')\
-        .replace('ê', 'e')\
-        .replace('ë', 'e')\
-        .replace('î', 'i')\
-        .replace('ï', 'i')\
-        .replace('ô', 'o')\
-        .replace('ö', 'o')\
-        .replace('ó', 'o')\
-        .replace('û', 'u')\
-        .replace('ü', 'u')
-
-    return base_url + name
 
 
 def get_info_from_url(link):
---
+++ MakeUrl.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+
+
+def main():
+    make_url_file('./data/rym_extract')
+
+
+def make_url_file(path):
+    rym = pd.read_csv(path, encoding='utf-8')
+    rym.columns = rym.columns.map(lambda x: x.strip(' '))
+    rym = rym.iloc[:, 1:3]
+    rym['First Name'] = rym['First Name'].apply(str)
+    rym = rym.drop_duplicates()
+
+    artists = rym.apply(lambda row: row[0] + ' ' + row[1], axis=1)
+    artists = artists.map(lambda x: x.replace('nan ', ''))
+
+    urls = artists.map(lambda x: make_url(x))
+    urls.to_csv('./data/urls', header=False, index=False)
+
+
+def make_url(band_name):
+    base_url = "https://rateyourmusic.com/artist/"
+
+    name = band_name\
+        .replace(' O)))', '-o') \
+        .replace(' ', '_') \
+        .replace('/', '_') \
+        .replace(',', '_') \
+        .replace('&amp;', 'and') \
+        .lower()\
+        .replace("'", "")\
+        .replace('à', 'a')\
+        .replace('â', 'a')\
+        .replace('ä', 'a')\
+        .replace('è', 'e')\
+        .replace('é', 'e')\
+        .replace('ê', 'e')\
+        .replace('ë', 'e')\
+        .replace('î', 'i')\
+        .replace('ï', 'i')\
+        .replace('ô', 'o')\
+        .replace('ö', 'o')\
+        .replace('ó', 'o')\
+        .replace('û', 'u')\
+        .replace('ü', 'u')
+
+    return base_url + name
+
+
+if __name__ == '__main__':
+    main()
