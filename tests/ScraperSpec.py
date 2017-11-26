9f23b39bad12ae97705ef97cdf91339470e36f91
Timothée <timothee.aupetit@gmail.com>
2017-11-26 19:35:02 +0100

    formatted code

--- tests/ScraperSpec.py
+++ tests/ScraperSpec.py
@@ -4,7 +4,10 @@ import Scraper
 
 class TestMember(unittest.TestCase):
     def test_empty(self):
-        self.assertEqual(Scraper.member_to_dict(""), None)
+        self.assertEqual(
+            Scraper.member_to_dict(""),
+            None
+        )
 
     def test_trailing_spaces(self):
         self.assertEqual(
@@ -16,6 +19,12 @@ class TestMember(unittest.TestCase):
         self.assertEqual(
             Scraper.member_to_dict("Stephen O'Malley"),
             {'Name': "Stephen O'Malley"}
+        )
+
+    def test_name_empty_aka(self):
+        self.assertEqual(
+            Scraper.member_to_dict('Joey Jordison []'),
+            {'Name': 'Joey Jordison'}
         )
 
     def test_name_aka(self):
@@ -36,15 +45,9 @@ class TestMember(unittest.TestCase):
             {'Name': 'Joey Jordison', 'Instruments': 'drums', 'Aka': '#1'}
         )
 
-    def test_name_empty_instruments(self):
+    def test_name_empty_features(self):
         self.assertEqual(
             Scraper.member_to_dict('Joey Jordison ()'),
-            {'Name': 'Joey Jordison'}
-        )
-
-    def test_name_empty_aka(self):
-        self.assertEqual(
-            Scraper.member_to_dict('Joey Jordison []'),
             {'Name': 'Joey Jordison'}
         )
 
@@ -59,51 +62,84 @@ class TestPeriod(unittest.TestCase):
     """
     Tests if string representing periods are correctly transformed in dict
     """
-    def test00_no_date(self):
+    def test_empty(self):
         """Empty input returns a dictionary with empty string values"""
-        self.assertEqual(Scraper.transform_period(''), {'Start': '', 'End': ''})
+        self.assertEqual(
+            Scraper.transform_period(''),
+            None
+        )
 
-    def test01_single_date1(self):
+    def test_single_date1(self):
         """A single 4-digits year gives Start and End"""
-        self.assertEqual(Scraper.transform_period('1984'), {'Start': 1984, 'End': 1984})
+        self.assertEqual(
+            Scraper.transform_period('1984'),
+            {'Start': 1984, 'End': 1984}
+        )
 
-    def test02_single_date2(self):
+    def test_single_date2(self):
         """A single 4-digits year gives Start and End"""
-        self.assertEqual(Scraper.transform_period('2001'), {'Start': 2001, 'End': 2001})
+        self.assertEqual(
+            Scraper.transform_period('2001'),
+            {'Start': 2001, 'End': 2001}
+        )
 
-    def test03_present(self):
+    def test_present(self):
         """Test if string end year is correctly transformed in empty string"""
-        self.assertEqual(Scraper.transform_period('2005-present'), {'Start': 2005, 'End': ''})
+        self.assertEqual(
+            Scraper.transform_period('2005-present'),
+            {'Start': 2005, 'End': ''}
+        )
 
-    def test04_partial_1900(self):
+    def test_partial_1900(self):
         """Test if year on 2 digits is correctly formed"""
-        self.assertEqual(Scraper.transform_period('1992-95'), {'Start': 1992, 'End': 1995})
+        self.assertEqual(
+            Scraper.transform_period('1992-95'),
+            {'Start': 1992, 'End': 1995}
+        )
 
-    def test05_partial_2000(self):
+    def test_partial_2000(self):
         """Test if year on 2 digits is correctly formed"""
-        self.assertEqual(Scraper.transform_period('2002-04'), {'Start': 2002, 'End': 2004})
+        self.assertEqual(
+            Scraper.transform_period('2002-04'),
+            {'Start': 2002, 'End': 2004}
+        )
 
-    def test05_partial_2010(self):
+    def test_partial_2010(self):
         """Test if year on 2 digits is correctly formed"""
-        self.assertEqual(Scraper.transform_period('2011-16'), {'Start': 2011, 'End': 2016})
+        self.assertEqual(
+            Scraper.transform_period('2011-16'),
+            {'Start': 2011, 'End': 2016}
+        )
 
-    def test06_complete_dates(self):
+    def test_complete_dates(self):
         """4-digit years on both sides"""
-        self.assertEqual(Scraper.transform_period('1999-2004'), {'Start': 1999, 'End': 2004})
+        self.assertEqual(
+            Scraper.transform_period('1999-2004'),
+            {'Start': 1999, 'End': 2004}
+        )
 
-    def test07_interrogation_1(self):
+    def test_interrogation_1(self):
         """End year is correctly transformed in empty string"""
-        self.assertEqual(Scraper.transform_period('2005-?'), {'Start': 2005, 'End': ''})
+        self.assertEqual(
+            Scraper.transform_period('2005-?'),
+            {'Start': 2005, 'End': ''}
+        )
 
-    def test08_interrogation_2(self):
+    def test_interrogation_2(self):
         """Start year is correctly transformed in empty string"""
-        self.assertEqual(Scraper.transform_period('?-2005'), {'Start': '', 'End': 2005})
+        self.assertEqual(
+            Scraper.transform_period('?-2005'),
+            {'Start': '', 'End': 2005}
+        )
 
 
 class TestPeriods(unittest.TestCase):
-    def test00_empty(self):
+    def test_empty(self):
         """Empty list of periods returns an empty list"""
-        self.assertEqual(Scraper.transform_periods([]), [])
+        self.assertEqual(
+            Scraper.transform_periods(''),
+            None
+        )
 
 
 if __name__ == '__main__':
